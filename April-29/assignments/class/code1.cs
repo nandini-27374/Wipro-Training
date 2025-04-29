29b73f20c4bd6dcf10ac4cb443dcfe3a5d801286
Nandini G <gurikaninandini@gmail.com>
2025-04-29 19:26:45 +0530

    class

---
+++ April-29/assignments/class/code1.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int age=25;
+        string name="Nandini";
+        bool student=true;
+        double fees=55000;
+        
+        Console.WriteLine ("age: "+age);
+        Console.WriteLine ("name: "+name);
+        Console.WriteLine ("student: "+student);
+        Console.WriteLine ("fees: "+fees);
+    }
+}
