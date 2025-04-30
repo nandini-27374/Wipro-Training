94f32accf74d40094296e22725853498825ddd7f
Nandini G <gurikaninandini@gmail.com>
2025-04-30 10:45:23 +0530

    code4

---
+++ April-30/class/classcode4.cs
@@ -0,0 +1,13 @@
+using System;
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int i=5;
+        while (i>=1) 
+        {
+            Console.WriteLine(i);
+            i--;
+        }
+    }
+}
