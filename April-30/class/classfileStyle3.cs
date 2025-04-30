190ec4baa28626be536a963acd20c09d3577d2de
Nandini G <gurikaninandini@gmail.com>
2025-04-30 13:03:58 +0530

    classcode9

---
+++ April-30/class/classfileStyle3.cs
@@ -0,0 +1,12 @@
+using System;
+class Program
+{
+    static void PrintMessage()
+    {
+        Console.WriteLine("Welcome to ChatGPT AI");
+    }
+    static void Main()
+    {
+        PrintMessage();
+    }
+}
