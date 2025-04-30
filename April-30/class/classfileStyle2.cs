133ba02a80d577cb8a783de1fd45c7c5c0026a43
Nandini G <gurikaninandini@gmail.com>
2025-04-30 13:02:34 +0530

    code8

---
+++ April-30/class/classfileStyle2.cs
@@ -0,0 +1,12 @@
+using System;
+class Program
+{
+    static void GreetUser(string name)
+    {
+        Console.WriteLine("Hello, "+name);
+    }
+    static void Main()
+    {
+        GreetUser("Nandini");
+    }
+}
