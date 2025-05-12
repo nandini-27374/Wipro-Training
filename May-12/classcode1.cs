4b7a324ff825d889e39aa71ea9144f72964fef82
nandini-27374 <gurikaninandini26@gmail.com>
2025-05-12 09:36:44 +0530

    Classcode1

---
+++ May-12/classcode1.cs
@@ -0,0 +1,21 @@
+using System;
+interface IShape
+{
+    void Draw();
+}
+class Circle : IShape
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing a Circle");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Circle circle=new Circle();
+        circle.Draw();
+    }
+}
