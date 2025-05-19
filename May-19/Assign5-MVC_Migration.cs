8d5318c33fe685dba2c0e39b6b5b7cfb67c0d6e0
nandini-27374 <gurikaninandini26@gmail.com>
2025-05-19 18:27:20 +0530

    Assign5_MVC_Migration

---
+++ May-19/Assign5-MVC_Migration.cs
@@ -0,0 +1,207 @@
+//create a new webapllication mvc project
+
+// right click-->on models -->create Product.cs class
+
+//inside that write below code
+
+/* 
+
+using System.ComponentModel;
+
+namespace WebApplication8.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
+*/
+
+//create Controller for the student
+
+//in Package manager console do this below commands 1. Add-Migration InitialCreate 2. Update-Database
+
+/*
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication8.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
+*/
+
+//see the wether the table is created or not  in the SQL Server Object Explorer by refreshing it
+
+//again give the below program
+
+
+using System.ComponentModel;
+
+namespace WebApplication8.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Category Category { get; set; }
+    }
+
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
+
+
+//give the cammand as below
+
+// Add-Migration -Name RelationshipMigration -Context WebApplication8Context
+
+// Update-Database -Migration RelationshipMigration -Context WebApplication8Context
+
+/*
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication8.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelationshipMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_CategoryId",
+                table: "Product",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Category_CategoryId",
+                table: "Product",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Category_CategoryId",
+                table: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_CategoryId",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Product");
+        }
+    }
+}
+*/
+
+// see the wether the Grade column in the table is updated or not in the SQL Server Object Explorer after refreshing it
+
+//then give below commands
+
+// Add-Migration -Name RemoveCategoryMigration -Context WebApplication8Context
+
+// Update-Database -Migration RemoveCategoryMigration -Context WebApplication8Context
+/*
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication8.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCategoryMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
+
+*/
+
+//the grade column and its data will be losed in the table after refreshing the SQL Server Object Explorer
+
+
