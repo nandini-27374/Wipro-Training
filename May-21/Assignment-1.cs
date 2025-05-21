b1104922aa36e8a9c0de634b1282f989a955a60c
nandini-27374 <gurikaninandini26@gmail.com>
2025-05-21 13:12:59 +0530

    TowerOfHonoi

---
+++ May-21/Assignment-1.cs
@@ -0,0 +1,52 @@
+/*
+1. Write a method:
+void TowerOfHanoi(int n, char from, char to, char aux)
+2. Input:
+Number of disks n
+3. Output:
+Print the steps to move all disks from source to destination using
+auxiliary rod
+4. Example for n = 3 :
+Move disk 1 from A to C
+Move disk 2 from A to B
+Move disk 1 from C to B
+*/
+
+using System;
+
+class Program
+{
+    static void TowerOfHanoi(int n, char from, char to, char aux)
+    {
+        if (n == 1)
+        {
+            Console.WriteLine("Move disk 1 from " + from + " to " + to);
+            return;
+        }
+
+        TowerOfHanoi(n - 1, from, aux, to);
+        Console.WriteLine("Move disk " + n + " from " + from + " to " + to);
+        TowerOfHanoi(n - 1, aux, to, from);
+    }
+
+    static void Main()
+    {
+        int n = 3; 
+        TowerOfHanoi(n, 'A', 'C', 'B');
+    }
+}
+
+
+
+/*
+Output:
+
+Move disk 1 from A to C
+Move disk 2 from A to B
+Move disk 1 from C to B
+Move disk 3 from A to C
+Move disk 1 from B to A
+Move disk 2 from B to C
+Move disk 1 from A to C
+
+*/
