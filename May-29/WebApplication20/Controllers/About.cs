74602891e7b6f2eb3e1cdcb50dd0ac9460d2f147
nandini-27374 <gurikaninandini26@gmail.com>
2025-05-29 15:49:13 +0530

    Employee_WebCreation

---
+++ May-29/WebApplication20/Controllers/About.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication20.Controllers
+{
+    public class About : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ May-29/WebApplication20/Data/WebApplication20Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication20.Models;
+
+namespace WebApplication20.Data
+{
+    public class WebApplication20Context : DbContext
+    {
+        public WebApplication20Context (DbContextOptions<WebApplication20Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication20.Models.Employee> Employee { get; set; } = default!;
+    }
+}
