0faceeef6c89f8b7cb9cc709b7b8e2a0de0cd1c0
nandini-27374 <gurikaninandini26@gmail.com>
2025-06-02 17:42:22 +0530

    WebAPI_Exercise_Employee

---
+++ June-2/Exercise1_02-06-2025/Data/Exercise1_02_06_2025Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Exercise1_02_06_2025.Models;
+
+namespace Exercise1_02_06_2025.Data
+{
+    public class Exercise1_02_06_2025Context : DbContext
+    {
+        public Exercise1_02_06_2025Context (DbContextOptions<Exercise1_02_06_2025Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Exercise1_02_06_2025.Models.Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ June-2/Exercise1_02-06-2025/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise1_02_06_2025.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; } 
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; } 
+
+        public int Salary { get; set; }
+
+    }
+}
